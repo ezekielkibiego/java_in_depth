085d2cbfe7c018e9de42afdb09901f5da69ce9b1
Ezekiel Kibiego <kibiezekiel@gmail.com>
2022-11-24 11:09:56 +0300

    add overloading constructors

--- src/java_in_depth/Student.java
+++ src/java_in_depth/Student.java
@@ -1,69 +1,79 @@
 package java_in_depth;
 
-public class Student {
-	static int computeCount;
-	
-	int id;
-	String name;
-	String gender;
-	int age;
-	long phone; // java >7 --readability
-	char degree;
-	double gpa;
-	
-	boolean international;
-	double tuitionFees = 20000.0, internationalFees = 3000.0;
-	
-	Student(int newId, String newName, String newGender, int newAge, long newPhone, double newGpa, char newDegree, boolean isInternational){
-		id = newId;
-		name = newName;
-		gender = newGender;
-		age = newAge;
-		phone = newPhone;
-		gpa = newGpa;
-		degree = newDegree;
-		international = isInternational;
-		
-	}
-	
-//	long minValue = Long.MIN_VALUE;
-//	long maxValue = Long.MAX_VALUE;
-	
-	void compute() {
-		computeCount = computeCount + 1;
-		
-		if (international) {
-			tuitionFees = tuitionFees + internationalFees;
-		}
-		
-		System.out.println("id: " + id);
-		System.out.println("name: " + name);
-		System.out.println("gender: " + gender);
-		System.out.println("age: " + age);
-		System.out.println("phone: " + phone);
-		System.out.println("gpa: " + gpa);
-		System.out.println("degree: " + degree);
-		System.out.println("tuitionFees: " + tuitionFees);
-		System.out.println("computeCount: " + computeCount);
-//		System.out.println("minValue: " + minValue);
-//		System.out.println("maxValue: " + maxValue);
-	}
 
-	public static void main(String[] args) {
+class Student {
+      static int studentCount;
+	  
+      int id;
+	  String name;
+	  String gender;
+	  int age;
+	  long phone;	  
+	  double gpa;	  
+	  char degree;
+	  
+	  boolean international;
+	  double tuitionFees = 12000.0;
+	  double internationalFees = 5000.0;
+	  
+	  Student(int newId, String newName, String newGender, int newAge, long newPhone, double newGpa, 
+					char newDegree) {		  
+	      this(newId, newName, newGender, newAge, newPhone, newGpa, newDegree, false);		  
+	  }
+	  
+	  Student(int id, String name, String gender, int age, long phone, double gpa, 
+					char degree, boolean isInternational) {
+		  this.id = id;
+		  this.name = name;
+		  this.gender = gender;
+		  this.age = age;
+		  this.phone = phone;
+		  this.gpa = gpa;
+		  this.degree = degree;
+		  international = isInternational;
+		  
+		  studentCount = studentCount + 1;
+		  int nextId = id + 1;		  
+		  
+		  if (international) {
+		      tuitionFees = tuitionFees + internationalFees;
+			  //return;
+		  }	
+		  
+	      System.out.println("\nid: " + this.id);
+		  System.out.println("nextId: " + nextId);
+		  System.out.println("name: " + this.name);
+		  System.out.println("gender: " + this.gender);
+		  System.out.println("age: " + this.age);
+		  System.out.println("phone: " + this.phone);
+		  System.out.println("gpa: " + this.gpa);
+		  System.out.println("degree: " + this.degree);
+		  System.out.println("tuitionFees: " + tuitionFees);
+		  System.out.println("studentCount: " + studentCount);
+	  }
+	  
+	  Student() {}
 	
-		Student s1 = new Student(1000, "Ezekiel", "male", 21, 722_991_833L, 4.0, 'B', false  );
-		
-		s1.compute();
-		
-		System.out.println("   ");
-		
-		Student s2 = new Student(1001, "Mary", "female", 25, 722_991_222L, 4.2, 'M', true);
-		s2.compute();
-		
-		System.out.println("   ");
-		
-		Student s3 = new Student(1003, "Anita", "female", 24, 722_991_111L, 3.9, 'M', true);
-		
-		s3.compute();
-	}
-}
+	  boolean updateProfile (String name) {
+		  this.name = name;
+		  return true;
+	  }
+
+	  public static void main(String[] args) {
+	      Student student1 = new Student(1000, "Ezekiel", "male", 18, 72_456_7890L, 3.8, 'B');
+		  
+		  Student student2 = new Student(1001, "Mary", "female", 21, 72_456_9999L, 3.4, 'M', true);
+		  
+		  Student student3 = new Student(1002, "Anita", "female", 20, 72_456_8888L, 4.0, 'M', true);
+		  
+		  System.out.println("Student.studentCount: " + Student.studentCount);
+		  
+		  System.out.println("\nName of student 1: " + student1.name); 
+		  System.out.println("Name of student 2: " + student2.name);
+		  System.out.println("Name of student 3: " + student3.name);
+		  
+		  student1.updateProfile("Tom");
+		  System.out.println("\nUpdated name of student 1: " + student1.name);
+		  
+	  }
+  }
